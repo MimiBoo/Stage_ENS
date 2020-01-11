994e6ccd2aab46bd696542a1d37fa8bb30b0bedc
Melkia Mouhamed <melkia02@gmail.com>
2020-01-11 14:31:06 +0100

    finished the uploader

--- Assets/app/uploader.py
+++ Assets/app/uploader.py
@@ -5,26 +5,41 @@ import pyodbc
 def read(conn, firebase):
     print("Read")
     cursor = conn.cursor()
-    cursor.execute("select * from class")
-    for row in cursor:
+    points = cursor.execute("select * from Point")
+    for row in points:
+        students = cursor.execute(
+            f"select std_num, std_name, std_lastname from Student where std_num={row[6]}"
+        )
+        student = students.fetchone()
+        subjects = cursor.execute(
+            f"select subj_name from Subject where subj_num={row[7]}")
+        subject = subjects.fetchone()
+        teachers = cursor.execute(
+            f"select teach_name, teach_lastname from Teacher where teach_num={row[8]}"
+        )
+        teacher = teachers.fetchone()
         data = {
-            "class_num":row[0],
-            "class_name":row[1],
-            "div_name":row[2],
-            "spec_name":row[3],
+            "Std_num": student[0],
+            "Std_name": student[1] + ' ' + student[2],
+            "Subject_name": subject[0],
+            "Teacher_name": teacher[0] + ' ' + teacher[1],
+            'Exam1': row[1],
+            'Exam2': row[2],
+            'tp': row[3],
+            'td': row[4],
+            'Ratrapage': row[5],
         }
-        resault = firebase.post("/Class", data)
+        print(student)
+        resault = firebase.post("/Marks", data)
         print(resault)
 
     print('')
 
 
-conn = pyodbc.connect(
-    "Driver={SQL Server Native Client 11.0};"
-    "Server=MIMI\\SQLEXPRESS;"
-    "Database=Stage_ENS;"
-    "Trusted_Connection=yes;"
-    )
+conn = pyodbc.connect("Driver={SQL Server Native Client 11.0};"
+                      "Server=MIMI\\SQLEXPRESS;"
+                      "Database=Stage_ENS;"
+                      "Trusted_Connection=yes;")
 
 firebase = fb.FirebaseApplication("https://stage-9594d.firebaseio.com/", None)
 
