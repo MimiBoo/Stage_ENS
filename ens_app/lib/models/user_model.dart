8cf6c1c2ef849ae392426d46b5c7c227c57ab190
Melkia Mouhamed <melkia02@gmail.com>
2020-01-14 11:12:59 +0100

    ANdroid app files

---
+++ ens_app/lib/models/user_model.dart
@@ -0,0 +1,4 @@
+class UserData {
+  String user_id, password;
+  UserData(this.user_id, this.password);
+}
---
+++ ens_app/lib/views/notes_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class NotesPage extends StatefulWidget {
+  @override
+  _NotesPageState createState() => _NotesPageState();
+}
+
+class _NotesPageState extends State<NotesPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
